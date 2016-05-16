31d577fc6aa6e363c643499663f7254519876796
delphinus <delphinus@remora.cx>
2016-05-17 08:48:42 +0900

    Remove target glyphs

--- remove-glyphs.py
+++ remove-glyphs.py
@@ -20,6 +20,12 @@ with open(glyph_list, 'r') as f:
       glyph_codes.append(int(m.group(1), 16))
 
 source_font = fontforge.open(font_filename)
+
+for code in glyph_codes:
+  source_font.selection.select(('more', 'unicode'), code)
+  for glyph in source_font.selection.byGlyphs:
+    source_font.removeGlyph(glyph)
+
 fontname, fallbackStyle = re.match("^([^-]*).*?([^-]*(?!.*-))$", source_font.fontname).groups()
 new_fontname = fontname + ' Reduced'
 source_font.familyname = new_fontname
@@ -28,6 +34,8 @@ source_font.fontname = new_fontname.replace(' ', '') + '-' + fallbackStyle
 source_font.appendSFNTName('English (US)', 'Preferred Family', source_font.familyname)
 source_font.appendSFNTName('English (US)', 'Compatible Full', source_font.fullname)
 source_font.appendSFNTName('English (US)', 'SubFamily', fallbackStyle)
+
+
 dest = os.path.basename(font_filename).replace('.', '-Reduced.')
 source_font.generate(dest)
 print('{0} generated'.format(new_fontname))
